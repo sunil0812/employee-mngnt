ec80496b3537a4106f38e79870eb5f9de528ece2
sunil0812 <sunilkumarack2@gmail.com>
2025-03-04 22:18:54 +0530

    updated bdd, changed logic for update employee

--- src/test/java/com/employee/bdd/EmployeeSteps.java
+++ src/test/java/com/employee/bdd/EmployeeSteps.java
@@ -2,8 +2,8 @@ package com.employee.bdd;
 
 import com.employee.entity.request.EmployeeEntity;
 import com.employee.entity.response.BaseEmployeeResponse;
-import com.employee.entity.response.ManagerResponse;
 import com.employee.entity.response.TeamData;
+import com.employee.entity.response.UpdateResponse;
 import com.employee.model.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,27 +14,26 @@ import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
-import io.zonky.test.db.flyway.FlywayWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
 
 public class EmployeeSteps extends BaseSteps {
 
@@ -51,7 +50,7 @@ public class EmployeeSteps extends BaseSteps {
     public void employee_details_request(String role) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
-        EmployeeEntity request = mapper.readValue(new File("src/test/resources/stubs/" + role.toLowerCase() + "_request.json"), EmployeeEntity.class);
+        EmployeeEntity request = mapper.readValue(new File("src/test/resources/stubs/insert/" + role.toLowerCase() + "_request.json"), EmployeeEntity.class);
 
         request = request.getRole().contains("manager") ? request : getManagerId(request);
 
@@ -108,6 +107,35 @@ public class EmployeeSteps extends BaseSteps {
         int rowCount = 0;
         try (ResultSet resultSet = executeQuery(query)) {
             while (resultSet.next()) {
+                checkInTeamTable(empId, resultSet.getInt("team_id"));
+                rowCount++;
+            }
+        }
+        assertEquals(1, rowCount);
+    }
+
+    @Given("id {string} should available in database")
+    public void checkInDbByEmpId(String empId) throws SQLException {
+        String query = "select * from employee_message.employees_data where emp_id='" + empId + "'";
+        int rowCount = 0;
+        try (ResultSet resultSet = executeQuery(query)) {
+            while (resultSet.next()) {
+                rowCount++;
+            }
+        }
+        assertEquals(1, rowCount);
+    }
+
+    private void checkInTeamTable(String empId, int teamId) throws SQLException {
+        String query = "select * from employee_message.team_data where id='" + teamId + "'";
+        int rowCount = 0;
+        try (ResultSet resultSet = executeQuery(query)) {
+            while (resultSet.next()) {
+                Array value = resultSet.getArray("team_members");
+                String[] members = (String[]) value.getArray();
+                List<String> values = List.of(members);
+                assertTrue(members.length > 0);
+                assertTrue(values.contains(empId));
                 rowCount++;
             }
         }
@@ -130,7 +158,6 @@ public class EmployeeSteps extends BaseSteps {
     public void validateResponse(String empId, String teamName, String role) throws SQLException, JsonProcessingException {
         String value = response.extract().body().asPrettyString();
         BaseEmployeeResponse actualResponse = mapper.readValue(value, BaseEmployeeResponse.class);
-        System.out.println(" response" + actualResponse);
         assertEquals(200, response.extract().statusCode());
         String query = "select * from employee_message.employees_data where emp_id='" + empId + "'";
         int rowCount = 0;
@@ -156,14 +183,90 @@ public class EmployeeSteps extends BaseSteps {
         assertEquals(1, rowCount);
     }
 
+    @When("employee id {string} removed from a team with status {string}")
+    public void updateEmployee(String empId, String status) {
+
+        RestAssured.baseURI = "http://localhost:8080";
+        response = RestAssured.given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .when().put("emp/update/{empId}/{status}", empId, status)
+                .then()
+                .log().all();
+    }
+
+    @Then("validate the id {string} details with removed team")
+    public void validateUpdatedEmpResponse(String empId) throws SQLException, JsonProcessingException {
+        String value = response.extract().body().asPrettyString();
+
+        assertEquals(200, response.extract().statusCode());
+        String query = "select * from employee_message.employees_data where emp_id='" + empId + "'";
+        int rowCount = 0;
+        try (ResultSet resultSet = executeQuery(query)) {
+            while (resultSet.next()) {
+                rowCount++;
+                int team = resultSet.getInt("team_id");
+                assertNotEquals(team, 1);
+                assertEquals(" Employee Successfully Removed From Team EmpId: " + empId, value);
+            }
+        }
+
+        assertEquals(1, rowCount);
+    }
+
+    @When("update details for employee id {string} with new team")
+    public void updateEmployeeDetails(String empId) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeEntity request = mapper.readValue(new File("src/test/resources/stubs/update/" + empId.toLowerCase() + "_request.json"), EmployeeEntity.class);
+
+        RestAssured.baseURI = "http://localhost:8080";
+        response = RestAssured.given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when().put("emp/update/{empId}", empId)
+                .then()
+                .log().all();
+    }
+
+    @Then("validate the id {string} details with email as {string} and team")
+    public void validateUpdateEmpResponse(String empId, String email) throws SQLException, JsonProcessingException {
+        String value = response.extract().body().asPrettyString();
+        UpdateResponse actualResponse = mapper.readValue(value, UpdateResponse.class);
+        assertEquals(200, response.extract().statusCode());
+        String query = "select * from employee_message.employees_data where emp_id='" + empId + "'";
+        int rowCount = 0;
+        try (ResultSet resultSet = executeQuery(query)) {
+            while (resultSet.next()) {
+                rowCount++;
+                String mail = resultSet.getString("email");
+                int teamId = resultSet.getInt("team_id");
+                checkInTeamTable(empId, teamId);
+                assertEquals(mail, email);
+                assertEquals(1, teamId);
+                assertEquals("Updated", actualResponse.getStatus());
+            }
+        }
+        assertEquals(1, rowCount);
+    }
+
+    private void checkZeroTeamMembers(int teamId) throws SQLException {
+        String query = "select * from employee_message.team_data where id='" + teamId + "'";
+        int rowCount = 0;
+        try (ResultSet resultSet = executeQuery(query)) {
+            while (resultSet.next()) {
+                Array value = resultSet.getArray("team_members");
+                String[] members = (String[]) value.getArray();
+                assertTrue(members.length == 0);
+                rowCount++;
+            }
+        }
+    }
+
     private ResultSet executeQuery(String query) throws SQLException {
         Connection connection = dataSource.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(query);
         return preparedStatement.executeQuery();
     }
 
-    public String readJsonFile(String fileName) throws IOException {
-        ClassPathResource resource = new ClassPathResource(fileName);
-        return new String(Files.readAllBytes(resource.getFile().toPath()));
-    }
 }
